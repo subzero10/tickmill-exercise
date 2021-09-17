2d5a04aa27d816783c632b791b8578b65ab678db
Pangratios Cosma <pagcosma@gmail.com>
2021-09-17 17:30:14 +0300

    format date fields

--- src/domain/Users/index.tsx
+++ src/domain/Users/index.tsx
@@ -1,20 +1,42 @@
 import {Component} from "react";
-import {GridColumns} from '@mui/x-data-grid';
+import {GridColumns, GridValueFormatterParams} from '@mui/x-data-grid';
 import TableWithPagination from "../../components/TableWithPagination";
 import {User} from "./user.dto";
+
+function dateFormatter(params: GridValueFormatterParams): string {
+    return new Date(params.getValue(params.id, 'createdAt') as string).toLocaleString();
+}
 
 export default class Users extends Component<{}, {}> {
 
     getTableColumns(): GridColumns {
         return [
-            {field: 'firstName', headerName: 'First Name', flex: 1, sortable: true},
-            {field: 'lastName', headerName: 'Last Name', flex: 1, sortable: true},
-            {field: 'role', headerName: 'Role', flex: 1, sortable: false},
-            {field: 'email', headerName: 'Email', flex: 1, sortable: true},
-            {field: 'phone', headerName: 'Phone', flex: 1, sortable: true},
-            {field: 'avatar', headerName: 'Avatar', flex: 1, sortable: false},
-            {field: 'createdAt', headerName: 'Created At', flex: 1, sortable: true},
-            {field: 'updatedAt', headerName: 'Updated At', flex: 1, sortable: true},
+            {
+                field: 'firstName', headerName: 'First Name', flex: 1, sortable: true
+            },
+            {
+                field: 'lastName', headerName: 'Last Name', flex: 1, sortable: true
+            },
+            {
+                field: 'role', headerName: 'Role', flex: 1, sortable: false
+            },
+            {
+                field: 'email', headerName: 'Email', flex: 1, sortable: true
+            },
+            {
+                field: 'phone', headerName: 'Phone', flex: 1, sortable: true
+            },
+            {
+                field: 'avatar', headerName: 'Avatar', flex: 1, sortable: false
+            },
+            {
+                field: 'createdAt', headerName: 'Created At',
+                flex: 1, sortable: true, valueFormatter: dateFormatter
+            },
+            {
+                field: 'updatedAt', headerName: 'Updated At',
+                flex: 1, sortable: true, valueFormatter: dateFormatter
+            },
         ];
     }
 
